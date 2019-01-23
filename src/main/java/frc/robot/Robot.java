42693e645405f914b689c24943c30cd7fd612af0
Moshe Chelsky <Moshechelsky@gmail.com>
2019-01-23 23:04:13 +0200

    Fixed based on other code review
    - comments
    ~ changed names
    ~ fixed bug

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Solenoid;
@@ -19,20 +12,15 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  public static HatchCollector hatchCollector;
+  public HatchCollector hatchCollector;
 
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    hatchCollector = new HatchCollector(new VictorSP(RobotMap.HATCH_COLLECTOR_MOTOR),
-        Solenoid(RobotMap.HATCH_COLLECTOR_SOLENOID));
-  }
-
-  // a error showed up without this
-  private Solenoid Solenoid(int i) {
-    return null;
+    this.hatchCollector = new HatchCollector(new VictorSP(RobotMap.HATCH_COLLECTOR_MOTOR),
+    new Solenoid(RobotMap.HATCH_COLLECTOR_SOLENOID));
   }
 
   @Override
@@ -42,7 +30,6 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
   }
 
@@ -50,11 +37,9 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
     case kCustomAuto:
-      // Put custom auto code here
       break;
     case kDefaultAuto:
     default:
-      // Put default auto code here
       break;
     }
   }
--- src/main/java/frc/robot/SubSystems/HatchCollector.java
+++ src/main/java/frc/robot/SubSystems/HatchCollector.java
@@ -1,38 +1,27 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.SubSystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * Add your docs here.
- */
 public class HatchCollector extends Subsystem {
-  Solenoid hatchCollectorSolenoid;
+  Solenoid solenoid;
   //if there is no image procassing then we use a motor
-  SpeedController hatchCollectorMotor;
+  SpeedController motor;
 
-  public HatchCollector(SpeedController hatchCollectorMotor, Solenoid hatchCollectorSolenoid){
-    this.hatchCollectorSolenoid = hatchCollectorSolenoid;
-    this.hatchCollectorMotor = hatchCollectorMotor;
+  public HatchCollector(SpeedController motor, Solenoid solenoid){
+    this.solenoid = solenoid;
+    this.motor = motor;
   }
 
-  public void setSolenoid(Boolean solenoidOn){
-    this.hatchCollectorSolenoid.set(solenoidOn);
+  public void setSolenoid(Boolean on){
+    this.solenoid.set(on);
   }
-  public void setMotor(Double power){
-    this.hatchCollectorMotor.set(power);
+  public void setMotor(Double speed){
+    this.motor.set(speed);
   }
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+
   }
 }
