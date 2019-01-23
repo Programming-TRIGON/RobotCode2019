7ab270c5ac97aa1ed65a22330aed69ae21d75385
Moshe Chelsky <Moshechelsky@gmail.com>
2019-01-23 22:48:31 +0200

    Fixed based on other code review
    - comments
    ~ changed names

--- src/main/java/frc/robot/SubSystems/CargoFlip.java
+++ src/main/java/frc/robot/SubSystems/CargoFlip.java
@@ -1,34 +1,23 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.SubSystems;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * Add your docs here.
- */
 public class CargoFlip extends Subsystem {
-  SpeedController cargoFlipRightMotor, cargoFlipLeftMotor;
-  SpeedControllerGroup cargoFlip;
-  public CargoFlip(SpeedController cargoFlipRightMotor, SpeedController cargoFlipLeftMotor){
-    this.cargoFlipRightMotor = cargoFlipRightMotor;
-    this.cargoFlipLeftMotor = cargoFlipLeftMotor;
-    this.cargoFlip = new SpeedControllerGroup(this.cargoFlipRightMotor, this.cargoFlipLeftMotor);
+  SpeedController rightMotor, leftMotor;
+  SpeedControllerGroup motorGroup;
+  public CargoFlip(SpeedController rightMotor, SpeedController leftMotor){
+    this.rightMotor = rightMotor;
+    this.leftMotor = leftMotor;
+    this.motorGroup = new SpeedControllerGroup(this.rightMotor, this.leftMotor);
   }
   public void setFlip(Double speed){
-    this.cargoFlip.set(speed);
+    this.motorGroup.set(speed);
   }
 
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+
   }
 }
