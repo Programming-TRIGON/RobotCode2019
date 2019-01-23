ba598b5c1e44cb1796ac61ad206185a913ee77aa
Moshe Chelsky <Moshechelsky@gmail.com>
2019-01-23 22:52:10 +0200

    Fixed based on other code review
    - comments
    ~ changed names

--- src/main/java/frc/robot/SubSystem/OneEighty.java
+++ src/main/java/frc/robot/SubSystem/OneEighty.java
@@ -1,23 +1,15 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.SubSystem;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-
 public class OneEighty extends Subsystem {
-  SpeedController oneEightyMotor;
-  public OneEighty(SpeedController oneEightyMotor){
-    this.oneEightyMotor = oneEightyMotor;
+  SpeedController motor;
+  public OneEighty(SpeedController motor){
+    this.motor = motor;
   }
-  public void setOneEighty(double power){
-    this.oneEightyMotor.set(power);
+  public void setOneEighty(Double speed){
+    this.motor.set(speed);
   }
 
   @Override
