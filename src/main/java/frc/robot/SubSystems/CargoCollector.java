104fb0b41f82fc1d83e5d3c0119439b6307bbc03
Moshe Chelsky <Moshechelsky@gmail.com>
2019-01-23 22:44:10 +0200

    Fixed upon other code review
    - comments

--- src/main/java/frc/robot/SubSystems/CargoCollector.java
+++ src/main/java/frc/robot/SubSystems/CargoCollector.java
@@ -1,18 +1,8 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.SubSystems;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- * Add your docs here.
- */
 public class CargoCollector extends Subsystem {
   SpeedController cargoCollectorMotor;
   public CargoCollector(SpeedController cargoCollectorMotor){
@@ -24,7 +14,6 @@ public class CargoCollector extends Subsystem {
 
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+
   }
 }
