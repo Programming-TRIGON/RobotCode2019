caee8dfdad76dd21fe4016e69a3d266bb80da626
Moshe Chelsky <Moshechelsky@gmail.com>
2019-01-30 17:16:19 +0200

    bug

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
     private XboxController xbox; 
+    private Button buttonA;
     public OI(){
         this.xbox = new XboxController(0);
         this.buttonA = new JoystickButton(this.xbox, 1);
