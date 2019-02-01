1b89c445eed6bd916d8a2bbdb736dfa3d1dbd547
Eliya Fishman <Eliyafish@gmail.com>
2019-02-01 02:55:23 +0200

    Update OneEightyCmdG.java

--- src/main/java/frc/robot/CommandGroups/OneEightyCmdG.java
+++ src/main/java/frc/robot/CommandGroups/OneEightyCmdG.java
@@ -10,6 +10,7 @@ package frc.robot.CommandGroups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 import frc.robot.RobotConstants;
+import frc.robot.Commands.SetAngle;
 import frc.robot.Commands.SetLiftHeight;
 import frc.robot.RobotConstants.RobotDimensions.Angle;
 
@@ -23,22 +24,7 @@ double angle;
       if(Robot.lift.getHeight() < RobotConstants.RobotDimensions.Height.kOneEightySafety.key){
         addSequential(new SetLiftHeight(RobotConstants.RobotDimensions.Height.kOneEightySafety));
       }
+      addSequential(new SetAngle(this.angle));
     }
-    // Add Commands here:
-    // e.g. addSequential(new Command1());
-    // addSequential(new Command2());
-    // these will run in order.
-
-    // To run multiple commands at the same time,
-    // use addParallel()
-    // e.g. addParallel(new Command1());
-    // addSequential(new Command2());
-    // Command1 and Command2 will run in parallel.
-
-    // A command group will require all of the subsystems that each member
-    // would require.
-    // e.g. if Command1 requires chassis, and Command2 requires arm,
-    // a CommandGroup containing them would require both the chassis and the
-    // arm.
   }
 }
