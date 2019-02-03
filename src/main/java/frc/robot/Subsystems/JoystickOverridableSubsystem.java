5e878585d68a853970b9f98c6ee601de5b793e65
esh4 <eshel.sinclair@gmail.com>
2019-02-03 19:04:10 +0200

    issue #33

--- src/main/java/frc/robot/Subsystems/JoystickOverridableSubsystem.java
+++ src/main/java/frc/robot/Subsystems/JoystickOverridableSubsystem.java
@@ -2,6 +2,10 @@ package frc.robot.Subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/*
+* Class extended by subsystems we want to control with a joystick with a generic command.
+*/
+
 public abstract class JoystickOverridableSubsystem extends Subsystem{
     public abstract void move(double power);
 }
