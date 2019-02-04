2c54a841af57e794801fc20854beadc9dbcdff61
esh4 <eshel.sinclair@gmail.com>
2019-02-04 21:09:56 +0200

    resolved conflicts - actually tho

--- src/main/java/frc/robot/CommandGroups/CargoCollectCmdG.java
+++ src/main/java/frc/robot/CommandGroups/CargoCollectCmdG.java
@@ -1,5 +1,6 @@
 package frc.robot.CommandGroups;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.RobotConstants;
 import frc.robot.Commands.CollectCargo;
@@ -10,19 +11,16 @@ import frc.robot.RobotConstants.OneEightyAngle;
 import frc.robot.RobotConstants.LiftHeight;
 
 public class CargoCollectCmdG extends CommandGroup {
-  OneEightyAngle angle = RobotConstants.OneEightyAngle.kStraight;
   double COLLECTOR_POWER = 0.5;
   double HOLDER_POWER = 0.7;
-  LiftHeight height;
 //TODO: real values.
   public CargoCollectCmdG() {
-    this.height = RobotConstants.LiftHeight.kLiftBottom;
     /** starts by unfolding the crago collecter */
-    addSequential(new SetCargoFolderState(angle));
+    addSequential(new SetCargoFolderState(Value.kReverse));
     /**turn to the required angle */
-    addSequential(new SetOneEightyAngle(this.angle));
+    addSequential(new SetOneEightyAngle(RobotConstants.OneEightyAngle.kStraight));
     /** set lift height to bottom in order to collect cargo */
-    addSequential(new SetLiftHeight(height));
+    addSequential(new SetLiftHeight(RobotConstants.LiftHeight.kLiftBottom));
     /** collects the cargo */
     addSequential(new CollectCargo(this.COLLECTOR_POWER, this.HOLDER_POWER));
   }
