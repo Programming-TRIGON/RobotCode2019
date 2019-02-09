155f1d2076cba36879880f04d82849c3e89164b3
Moshe Chelsky <Moshechelsky@gmail.com>
2019-02-09 22:47:25 +0200

    documentation
    - TODO fix that

--- src/main/java/frc/robot/Commands/PathFinder.java
+++ src/main/java/frc/robot/Commands/PathFinder.java
@@ -11,7 +11,7 @@ import jaci.pathfinder.followers.EncoderFollower;
 
 public class PathFinder extends Command {
   // TODO: make real values
-  private double desired_heading, heading, heading_difference, turn, right_speed, left_speed;
+  private double desired_heading, heading, heading_difference, turn, right_speed, left_speed, kP, kI, kD, kA, turnSize;
   private EncoderFollower encoderFollowerLeft, encoderFollowerRight;
   private Trajectory leftTrajectory, rightTrajectory;
   private String pathName;
@@ -19,38 +19,68 @@ public class PathFinder extends Command {
   public PathFinder(String pathName) {
     requires(Robot.driveTrain);
     this.pathName = pathName;
+    this.kP = 0.2;
+    this.kI = 0;
+    this.kD = 0;
+    this.kA = 0;
+    this.turnSize = -0.01;
   }
 
   @Override
   protected void initialize() {
-
+    /** reseting sensors */
+    RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_RIGHT.reset();
+    RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_LEFT.reset();
+    RobotComponents.DriveTrain.DRIVETRAIN_GYRO.reset();
+    /**
+     * The tarjectory is the path(each side of the drivetrain has one) this is ike
+     * the "error" in PID
+     */
     this.rightTrajectory = PathfinderFRC.getTrajectory(this.pathName + ".right");
     this.leftTrajectory = PathfinderFRC.getTrajectory(this.pathName + ".left");
-
+    /**
+     * The encoder follower does PID using encoder input to find the motor power
+     */
     this.encoderFollowerLeft = new EncoderFollower(this.leftTrajectory);
     this.encoderFollowerRight = new EncoderFollower(this.rightTrajectory);
+    /**
+     * giving the encoderFollowers the current ticks and the CPR and wheelDiamter as
+     * we don't want it sto bbe in ticks we want a usable measurment
+     */
     encoderFollowerLeft.configureEncoder(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_LEFT.get(),
         RobotConstants.PathFinder.TicksPerRevolution, RobotConstants.PathFinder.WheelDiameter);
     encoderFollowerRight.configureEncoder(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_RIGHT.get(),
         RobotConstants.PathFinder.TicksPerRevolution, RobotConstants.PathFinder.WheelDiameter);
-    encoderFollowerLeft.configurePIDVA(1.0, 0.0, 0.0, 1 / RobotConstants.PathFinder.MaxVelocity, 0);
-    RobotComponents.DriveTrain.DRIVETRAIN_GYRO.reset();
+    /** setting the PID values: kp, ki,kd, kv, ka */
+    encoderFollowerLeft.configurePIDVA(this.kP, this.kI, this.kD, 1 / RobotConstants.PathFinder.MaxVelocity, this.kA);
   }
 
   @Override
   protected void execute() {
-
+    /** PID on encoders */
     this.left_speed = encoderFollowerLeft.calculate(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_LEFT.get());
     this.right_speed = encoderFollowerRight.calculate(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_RIGHT.get());
+    /** getting the current angle */
     this.heading = RobotComponents.DriveTrain.DRIVETRAIN_GYRO.getAngle();
+    /** getting our desired angle */
     this.desired_heading = Pathfinder.r2d(encoderFollowerLeft.getHeading());
+    /**
+     * finding our desired speed. boundHalf: converts values to a number between 180
+     * - -180 so it doesn't end up turning over 180 degrees
+     */
     this.heading_difference = Pathfinder.boundHalfDegrees(this.desired_heading - this.heading);
-    this.turn = 0.8 * (-1.0 / 80.0) * this.heading_difference;
+    /**
+     * making the value smaller so it doesn't have as much of a affect as the
+     * encoders
+     */
+    this.turn = this.turnSize * this.heading_difference;
+    /** setting the numbers given by the PID to the motors */
     Robot.driveTrain.tankDrive((this.left_speed + this.turn), (this.right_speed - this.turn));
   }
 
   @Override
   protected boolean isFinished() {
+    /** checking if the path is finshed */
     return encoderFollowerLeft.isFinished() && encoderFollowerRight.isFinished();
   }
 
