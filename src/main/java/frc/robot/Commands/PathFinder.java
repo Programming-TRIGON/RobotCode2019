ecbcb4cae92ebd35f4a3660085a0f5bd92724209
Moshe Chelsky <Moshechelsky@gmail.com>
2019-02-11 20:14:26 +0200

    Small fixes

--- src/main/java/frc/robot/Commands/PathFinder.java
+++ src/main/java/frc/robot/Commands/PathFinder.java
@@ -11,7 +11,7 @@ import jaci.pathfinder.followers.EncoderFollower;
 
 public class PathFinder extends Command {
   // TODO: make real values
-  private double desired_heading, heading, heading_difference, turn, right_speed, left_speed;
+  private double desiredAngle, angle, angleDifference, turn, rightSpeed, leftSpeed;
   private double kP, kI, kD, kA, turnSize, maxVelocity;
   private EncoderFollower encoderFollowerLeft, encoderFollowerRight;
   private Trajectory leftTrajectory, rightTrajectory;
@@ -38,47 +38,53 @@ public class PathFinder extends Command {
      * The tarjectory is the path(each side of the drivetrain has one) this is like
      * the "error" in PID
      */
-    this.rightTrajectory = PathfinderFRC.getTrajectory(this.pathName + ".right");
-    this.leftTrajectory = PathfinderFRC.getTrajectory(this.pathName + ".left");
+    // TODO: check when this bug is fixed (wpilibs side)
+    this.leftTrajectory = PathfinderFRC.getTrajectory(this.pathName + ".right");
+    this.rightTrajectory = PathfinderFRC.getTrajectory(this.pathName + ".left");
     /**
      * The encoder follower does PID using encoder input to find the motor power
      */
     this.encoderFollowerLeft = new EncoderFollower(this.leftTrajectory);
     this.encoderFollowerRight = new EncoderFollower(this.rightTrajectory);
     /**
-     * giving the encoderFollowers the current ticks and the CPR and wheelDiamter as
-     * we don't want it sto bbe in ticks we want a usable measurment
+     * giving the encoderFollowers the current ticks (starting point) and the CPR
+     * and wheelDiamter as we don't want it to be in ticks we want a usable
+     * measurment
      */
     encoderFollowerLeft.configureEncoder(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_LEFT.get(),
         RobotConstants.Sensors.TicksPerRevolution, RobotConstants.RobotDimensions.WheelDiameter);
     encoderFollowerRight.configureEncoder(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_RIGHT.get(),
         RobotConstants.Sensors.TicksPerRevolution, RobotConstants.RobotDimensions.WheelDiameter);
-    /** setting the PID values: kp, ki,kd, kv, ka */
+    /** setting the PID values: kp, ki, kd, kv, ka */
     encoderFollowerLeft.configurePIDVA(this.kP, this.kI, this.kD, 1 / this.maxVelocity, this.kA);
+    encoderFollowerRight.configurePIDVA(this.kP, this.kI, this.kD, 1 / this.maxVelocity, this.kA);
   }
 
   @Override
   protected void execute() {
     /** PID on encoders */
-    this.left_speed = encoderFollowerLeft.calculate(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_LEFT.get());
-    this.right_speed = encoderFollowerRight.calculate(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_RIGHT.get());
+    this.leftSpeed = encoderFollowerLeft.calculate(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_LEFT.get());
+    this.rightSpeed = encoderFollowerRight.calculate(RobotComponents.DriveTrain.DRIVETRAIN_ENCODER_RIGHT.get());
     /** getting the current angle */
-    this.heading = RobotComponents.DriveTrain.DRIVETRAIN_GYRO.getAngle();
-    /** getting our desired angle */
-    this.desired_heading = Pathfinder.r2d(encoderFollowerLeft.getHeading());
+    this.angle = RobotComponents.DriveTrain.DRIVETRAIN_GYRO.getAngle();
     /**
-     * finding our desired speed. boundHalf: converts values to a number between 180
-     * - -180 so it doesn't end up turning over 180 degrees
+     * getting our desired angle. we use only the left path because both sides of
+     * the robot are parallel
      */
-    this.heading_difference = Pathfinder.boundHalfDegrees(this.desired_heading - this.heading);
+    this.desiredAngle = Pathfinder.r2d(encoderFollowerLeft.getHeading());
     /**
-     * making the value smaller so it doesn't have as much of a affect as the
-     * encoders
+     * getting the anle the robot needs to move. boundHalf: converts values to a
+     * number between 180 - -180 so it doesn't end up turning over 180 degrees
      */
-    // TODO: FIX THIS
-    this.turn = this.turnSize * this.heading_difference;
+    this.angleDifference = Pathfinder.boundHalfDegrees(this.desiredAngle - this.angle);
+    /**
+     * the output is returned in 180 - -180 degress we want it as a speed to insert
+     * into the motors
+     */
+    // TODO: Set real value
+    this.turn = this.turnSize * this.angleDifference;
     /** setting the numbers given by the PID to the motors */
-    Robot.driveTrain.tankDrive((this.left_speed + this.turn), (this.right_speed - this.turn));
+    Robot.driveTrain.tankDrive((this.leftSpeed + this.turn), (this.rightSpeed - this.turn));
   }
 
   @Override
