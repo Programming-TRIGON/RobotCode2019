89f48bc40a55bcaea10550083fd4f81562833885
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-02-12 19:13:42 +0200

    Revert "Merge branch 'UnitTests' of https://github.com/Programming-Trigon/2019RobotCode into UnitTests"
    This reverts commit dcb2dd20b4a9abd7315687856d8c2b4821c49100, reversing
    changes made to 3c30013851293f3acfd91fdd822572f7d08070b6.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -171,7 +171,6 @@ public class Robot extends TimedRobot {
   }
 
   private void addTests(){
-    testsChooser.addDefault("cargoFolderClosed", new SetCargoFolderState(Value.kReverse));
     // testsChooser.addOption("cargoRoller", new CargoRollerTest());
     // testsChooser.addOption("cargoHolder", new CargoHolderTest());
     testsChooser.addOption("cargoCollection", new CollectCargo(0.3, 0.3));
@@ -187,9 +186,5 @@ public class Robot extends TimedRobot {
 
     testsChooser.addOption("hatchCollectorOn", new setHatchCollectorState(Value.kForward));
     testsChooser.addOption("hatchCollectorOff", new setHatchCollectorState(Value.kReverse));
-
-    testsChooser.addOption("cargoFolderOpen", new SetCargoFolderState(Value.kForward));
-    testsChooser.addOption("cargoFolderClosed", new SetCargoFolderState(Value.kReverse));
-
   }
 }
