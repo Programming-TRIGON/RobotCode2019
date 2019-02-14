9f2b953c99fe2e3fe6ff71b7edaac15076e366b0
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-02-14 18:38:47 +0200

    added docs, middle to feeder

--- src/main/java/frc/robot/Autonomous/FirstHatch/ScoreHatchMiddle.java
+++ src/main/java/frc/robot/Autonomous/FirstHatch/ScoreHatchMiddle.java
@@ -31,21 +31,26 @@ public class ScoreHatchMiddle extends CommandGroup {
   final double TURN_45 = 45;
   final double SECOND_DISTANCE = 5;
   final double TARGET_TRACK_TIME = 5;
-
+  final double TURN_TO_FEEDER = -45;
+  final double DRIVE_TO_FEEDER = 4;
   
   public ScoreHatchMiddle(boolean isLeft) {
+    // drive a bit towards the cargo ship 
     addSequential(new DriveTankWithPID(Robot.driveTrain, RobotComponents.DriveTrain.LEFT_ENCODER,
     RobotComponents.DriveTrain.RIGHT_ENCODER, () -> FIRST_DISTANCE, () -> FIRST_DISTANCE, 
     RobotConstants.RobotPIDSettings.DRIVE_SETTINGS));  
 
+    // turn a bit
     addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO, 
     TURN_45 * (isLeft ? 1:-1), 
     RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
 
+    // diagonal drive in the cargo ship's general direction
     addSequential(new DriveTankWithPID(Robot.driveTrain, RobotComponents.DriveTrain.LEFT_ENCODER,
     RobotComponents.DriveTrain.RIGHT_ENCODER, () -> SECOND_DISTANCE, () -> SECOND_DISTANCE, 
     RobotConstants.RobotPIDSettings.DRIVE_SETTINGS));  
 
+    // face the cargo ship
     addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO, 
     -TURN_45 * (isLeft ? 1:-1), 
     RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
@@ -57,6 +62,19 @@ public class ScoreHatchMiddle extends CommandGroup {
 
     addSequential(new EjectHatch());
 
+    // Turn to face the feeder
+    addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO, 
+    TURN_TO_FEEDER * (isLeft ? 1:-1), 
+    RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
 
+    // drive to feeder 
+    addSequential(new DriveTankWithPID(Robot.driveTrain, RobotComponents.DriveTrain.LEFT_ENCODER,
+    RobotComponents.DriveTrain.RIGHT_ENCODER, () -> DRIVE_TO_FEEDER, 
+    RobotConstants.RobotPIDSettings.DRIVE_SETTINGS));  
+
+    // Turn to face the feeder
+    addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO, 
+    TURN_TO_FEEDER * (isLeft ? 1:-1), 
+    RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
   }
 }
