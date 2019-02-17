48d5277e2a77654fe6d225b8a8d732016fe0947d
Mboesn <45824003+Mboesn@users.noreply.github.com>
2019-02-17 16:12:01 +0200

    finished second hatch side

--- src/main/java/frc/robot/Autonomous/SecondHatch/SecondHatchSide.java
+++ src/main/java/frc/robot/Autonomous/SecondHatch/SecondHatchSide.java
@@ -7,6 +7,7 @@
 
 package frc.robot.Autonomous.SecondHatch;
 
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcadeWithPID;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveTankWithPID;
 import com.spikes2212.genericsubsystems.drivetrains.commands.OrientWithPID;
 
@@ -14,6 +15,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 import frc.robot.RobotComponents;
 import frc.robot.RobotConstants;
+import frc.robot.CommandGroups.ScoreHatch;
+import frc.robot.Vision.VisionPIDSource;
+import frc.robot.Vision.VisionPIDSource.VisionDirectionType;
+import frc.robot.Vision.VisionPIDSource.VisionTarget;
 
 public class SecondHatchSide extends CommandGroup {
 
@@ -39,14 +44,25 @@ public class SecondHatchSide extends CommandGroup {
     final double distanceToRocket = Math.sqrt(Math.pow(start_feederDistance, 2) + Math.pow(driveDistance.key, 2));
     final double TARGET_TRACK_TIME = 5;
 
-    addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO, TURN_TO_ROCKET  * (isLeft ? 1:-1) ,
-        RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
+    // turns to face the rocket for going to it from the feeder.
+    addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO,
+        TURN_TO_ROCKET * (isLeft ? 1 : -1), RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
 
+    // drive to the rocket
     addSequential(new DriveTankWithPID(Robot.driveTrain, RobotComponents.DriveTrain.LEFT_ENCODER,
         RobotComponents.DriveTrain.RIGHT_ENCODER, distanceToRocket, RobotConstants.RobotPIDSettings.DRIVE_SETTINGS));
 
-    addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO, TURN_TO_HATCH  * (isLeft ? 1:-1), RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
+    // turns to face the rocket for putting the hatch pannel
+    addSequential(new OrientWithPID(Robot.driveTrain, RobotComponents.DriveTrain.GYRO,
+        TURN_TO_HATCH * (isLeft ? 1 : -1), RobotConstants.RobotPIDSettings.TURN_SETTINGS, 360, true));
 
+   // Use vision to deliver the hatch
+   addSequential(new DriveArcadeWithPID(Robot.driveTrain, 
+   new VisionPIDSource(VisionTarget.kReflector, VisionDirectionType.x), 0, Robot.oi.getYLeft(), 
+   RobotConstants.RobotPIDSettings.REFLECTOR_TRACK_SETTINGS, 2000, false), TARGET_TRACK_TIME);
+
+  //  scores the hatch
+  addSequential(new ScoreHatch(RobotConstants.LiftHeight.kRocketMiddleHatch));
   }
 
 }
