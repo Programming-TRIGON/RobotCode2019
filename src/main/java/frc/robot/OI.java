39bc293ba626ae8faa8e2a5f93583709629a5fba
Eliya Fishman <Eliyafish@gmail.com>
2019-02-19 22:54:42 +0200

    added drivewithvision to oi

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,32 +7,33 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import frc.robot.CommandGroups.CargoCollectCmdG;
 import frc.robot.CommandGroups.CollectHatch;
 import frc.robot.Commands.CollectCargo;
+import frc.robot.Commands.DriveArcadeWithVision;
 import frc.robot.Commands.SetDriveInverted;
+import frc.robot.Vision.VisionPIDSource.VisionTarget;
 
 /**
  * Add your docs here.
  */
 
 public class OI {
-    public XboxController operatorXbox = new XboxController(0);  
+    public XboxController operatorXbox = new XboxController(0);
     public XboxController driverXbox = new XboxController(1);
     Button driverButtonY, driverButtonA, driverButtonB, driverButtonX;
-    Button operatorButtonX, operatorButtonY, operatorButtonLB, operatorButtonRB,
-    operatorButtonA, operatorButtonB;
-    
-    public OI(){
-        // driver buttons 
+    Button operatorButtonX, operatorButtonY, operatorButtonLB, operatorButtonRB, operatorButtonA, operatorButtonB;
+
+    public OI() {
+        // driver buttons
         this.driverButtonA = new JoystickButton(driverXbox, 1);
         this.driverButtonY = new JoystickButton(driverXbox, 4);
         this.driverButtonB = new JoystickButton(driverXbox, 2);
         this.driverButtonX = new JoystickButton(driverXbox, 3);
-        
-        //this.driverButtonB.whileHeld(new );
+        this.driverButtonB.whileHeld(new DriveArcadeWithVision(Robot.driveTrain, VisionTarget.kReflector, () -> 0.0,
+                () -> driverXbox.getY(Hand.kLeft), RobotConstants.RobotPIDSettings.VISION_TURN_SETTINGS, false));
         this.driverButtonA.whenPressed(new SetDriveInverted(true));
         this.driverButtonY.whenPressed(new SetDriveInverted(false));
         this.driverButtonX.whileHeld(new CollectHatch());
 
-        // operator buttons 
+        // operator buttons
         this.operatorButtonA = new JoystickButton(operatorXbox, 1);
         this.operatorButtonB = new JoystickButton(operatorXbox, 2);
         this.operatorButtonX = new JoystickButton(operatorXbox, 3);
@@ -41,5 +42,5 @@ public class OI {
         this.operatorButtonRB = new JoystickButton(operatorXbox, 6);
 
         this.operatorButtonA.whenPressed(new CargoCollectCmdG());
-    }   
+    }
 }
