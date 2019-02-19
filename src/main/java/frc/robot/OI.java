7adf459e7bb19c56d0821f20d58fdfdc1d39723c
Eliya Fishman <Eliyafish@gmail.com>
2019-02-20 01:06:37 +0200

    Vision imrovements

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.buttons.POVButton;
@@ -38,7 +39,7 @@ public class OI {
         this.driverButtonY = new JoystickButton(driverXbox, 4);
         this.driverButtonB = new JoystickButton(driverXbox, 2);
         this.driverButtonX = new JoystickButton(driverXbox, 3);
-        this.driverButtonB.whileHeld(new DriveArcadeWithVision(Robot.driveTrain, VisionTarget.kReflector, () -> 0.0,
+        this.driverButtonB.whileHeld(new DriveArcadeWithVision(Robot.driveTrain, VisionTarget.kReflectorForward, () -> 0.0,
                 () -> driverXbox.getY(Hand.kLeft), RobotConstants.RobotPIDSettings.VISION_TURN_SETTINGS, false));
         this.driverButtonA.whenPressed(new SetDriveInverted(true));
         this.driverButtonY.whenPressed(new SetDriveInverted(false));
