73eebdd394f76ab7312e304e8496859426c70df4
Eshel Sinclair <eshel.sinclair@gmail.com>
2019-02-20 02:44:16 +0200

    Fixing test bug

---
+++ src/main/java/frc/robot/Commands/HigherI.java
@@ -0,0 +1,16 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import frc.robot.Tests;
+
+public class HigherI extends InstantCommand {
+  public HigherI() {
+  }
+
+  @Override
+  protected void initialize() {
+    Tests.i++;
+    Tests.pickCommand(Tests.commands[Tests.i]);
+  }
+
+}
