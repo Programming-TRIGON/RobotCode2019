dfedd0979def8cda6af3a6d6570334a973f3cb00
HilelAmiel@gmail.com <35809525+HilelAmiel@users.noreply.github.com>
2019-02-25 18:54:33 +0200

    last push

--- src/main/java/frc/robot/CommandGroups/SetLiftHeight.java
+++ src/main/java/frc/robot/CommandGroups/SetLiftHeight.java
@@ -11,6 +11,7 @@ public class SetLiftHeight extends CommandGroup {
    */
   public SetLiftHeight(LiftHeight height) {
     addSequential(new ReachLiftHeight(height)); //this command will end when overraide lift state is true else it will do pid on the height given 
+    
     addSequential(new SetLiftOverride()); //this command will run when overraide lift state is true
   }
 }
