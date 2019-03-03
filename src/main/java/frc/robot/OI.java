8b8c7d0a9f195eac412da09ce013bbe9851560e2
Eshel Sinclair <eshel.sinclair@gmail.com>
2019-03-03 18:12:58 +0200

    fix

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -4,28 +4,19 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.buttons.POVButton;
-import frc.robot.CommandGroups.AfterHatchFeederPreparation;
-import frc.robot.CommandGroups.CargoCollectCmdG;
-import frc.robot.CommandGroups.DefenceMode;
-import frc.robot.CommandGroups.EjectHatch;
-import frc.robot.CommandGroups.CollectHatchFromFeeder;
-import frc.robot.Commands.CollectCargo;
-import frc.robot.Commands.LiftSwitchOverride;
-import frc.robot.Commands.OneEightyToggleOverride;
-import frc.robot.Commands.PushCargo;
-import frc.robot.Commands.ReachLiftHeight;
-import frc.robot.CommandGroups.PrepareToScore;
-import frc.robot.CommandGroups.Push;
-import frc.robot.CommandGroups.PushWhenLiftMoved;
-import frc.robot.CommandGroups.SetLiftHeight;
-import frc.robot.CommandGroups.SetOneEightyAngle;
-import frc.robot.Commands.SetDriveInverted;
-import frc.robot.Commands.SetHasCargo;
-import frc.robot.Commands.ToggleDriveInverted;
-import frc.robot.RobotComponents.OneEighty;
-import frc.robot.RobotConstants.LiftHeight;
-import frc.robot.RobotConstants.OneEightyAngle;
 import frc.robot.RobotConstants.PrepareToScoreHeight;
+import frc.robot.CargoCollectorCommands.CollectCargo;
+import frc.robot.OICommands.CollectCargoFromFloor;
+import frc.robot.Commands.SetHasCargo;
+import frc.robot.DrivingCommands.ToggleDriveInverted;
+import frc.robot.LiftCommands.LiftSwitchOverride;
+import frc.robot.OICommands.AfterHatchFeederPreparation;
+import frc.robot.OICommands.CollectHatchFromFeeder;
+import frc.robot.OICommands.DefenceMode;
+import frc.robot.OICommands.PrepareToScore;
+import frc.robot.OICommands.Push;
+import frc.robot.OICommands.PushWhenLiftMoved;
+import frc.robot.OneEightyCommands.OneEightyToggleOverride;
 
 public class OI {
     public XboxController operatorXbox = new XboxController(0);
@@ -74,7 +65,7 @@ public class OI {
         this.operatorRightPOVButton.whileHeld(new SetHasCargo(true)); 
         this.operatorLeftPOVButton.whileHeld(new SetHasCargo(false));
 
-        this.operatorButtonA.whenPressed(new CargoCollectCmdG());
+        this.operatorButtonA.whenPressed(new CollectCargoFromFloor());
         //that ensure that the cargo collector would stop spinning 
         //if CargoCollectCmdG would interapted or the drivers wont catch cargo  
         this.operatorButtonA.whenPressed(new CollectCargo(0,0));  
