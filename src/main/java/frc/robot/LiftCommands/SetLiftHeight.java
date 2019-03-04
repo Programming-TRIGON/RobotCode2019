0d2ba3dcac5d0fbe101528253836d199aa0da6fa
EliyaFishman <45822259+EliyaFishaman@users.noreply.github.com>
2019-03-04 21:06:19 +0200

    bag

--- src/main/java/frc/robot/LiftCommands/SetLiftHeight.java
+++ src/main/java/frc/robot/LiftCommands/SetLiftHeight.java
@@ -1,6 +1,7 @@
 package frc.robot.LiftCommands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.Commands.SetHeightIndex;
 import frc.robot.RobotConstants.LiftHeight;
 
 public class SetLiftHeight extends CommandGroup {
@@ -8,7 +9,7 @@ public class SetLiftHeight extends CommandGroup {
    * Set lift height with overraid option
    */
   public SetLiftHeight(LiftHeight height) {
-    addSequential(new SetLiftHeight(height)); //write index to robot states
+    addSequential(new SetHeightIndex(height)); //write index to robot states
     addSequential(new ReachLiftHeight(height)); // this command will end when overraide lift state is true else it will do pid on the height given
     addSequential(new SetLiftOverride()); // this command will run when overraide lift state is true
   }
