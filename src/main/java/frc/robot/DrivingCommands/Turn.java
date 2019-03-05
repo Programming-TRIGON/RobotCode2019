faaaf03b7b263498106537e6ade5e974ac262748
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2019-03-05 17:38:22 +0200

    Turn now resets the encoder

--- src/main/java/frc/robot/DrivingCommands/Turn.java
+++ src/main/java/frc/robot/DrivingCommands/Turn.java
@@ -8,13 +8,18 @@ import frc.robot.Commands.GyroPIDSource;
 import frc.robot.RobotConstants.RobotPIDSettings;
 
 public class Turn extends OrientWithPID {
-  static final GyroPIDSource GYRO_PID_SOURCE = new GyroPIDSource(); 
+  static final GyroPIDSource GYRO_PID_SOURCE = new GyroPIDSource();
   /**
    *
    * @param angle the setpoint which the Robot turns using PID.
    */
   public Turn(double angle) {
-    super(Robot.driveTrain, Turn.GYRO_PID_SOURCE,angle, RobotPIDSettings.TURN_SETTINGS, 360,
-        true);
+    super(Robot.driveTrain, Turn.GYRO_PID_SOURCE, angle, RobotPIDSettings.TURN_SETTINGS, 360, true);
+  }
+
+  @Override
+  protected void initialize() {
+    RobotComponents.DriveTrain.GYRO.reset();
+    super.initialize();
   }
 }
