7531f97ba0a227788b743b966964e3e4247feb47
EliyaFishman <45822259+EliyaFishaman@users.noreply.github.com>
2019-03-11 15:40:38 +0200

    conflicts resolving

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -95,6 +95,12 @@ public class Robot extends TimedRobot {
     NetworkTableEntry target = imageProcessingTable.getEntry("target");
     target.setString(VisionPIDSource.VisionTarget.kReflectorForward.toString());
 
+    this.a = new JoystickButton(Robot.oi.driverXbox, 8);
+    a.whenPressed(new InstantCommand(() -> {
+      Robot.oi.operatorXbox.setRumble(RumbleType.kLeftRumble, 1);
+      Robot.oi.operatorXbox.setRumble(RumbleType.kRightRumble, 1);
+    }));
+    
     Robot.dbc = new DashBoardController();
 
     /** creates the SS htach collector that collects hatch pannels */
